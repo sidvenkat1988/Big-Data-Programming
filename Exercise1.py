2f3c224f4564547d6cef9090caa5103cb00bafe6
sidvenkat1988 <sid.venkat1988@gmail.com>
2019-05-03 15:06:54 +0200

    Committing with comments

--- Exercise1.py
+++ Exercise1.py
@@ -2,6 +2,7 @@ S1 = "ABAZDC"
 S2 = "BACBAD"
 A = ""
 
+#Matching the earliest existence of S1[0] in S2
 if S1[0] == S2[0]:
     A = S2[0]
 elif S1[0] == S2[1]:
@@ -18,6 +19,7 @@ else:
     A = ''
 print(A)
 
+#Matching the earliest existence of S1[1] in S2
 if S1[1] == S2[0] and (S1[0] != S2[0] or S1[0] != S2[1]):
     A = A + S2[0]
 elif S1[1] == S2[1] and (S1[0] != S2[1] or S1[0] != S2[2]):
@@ -34,6 +36,7 @@ else:
     A = A
 print(A)
 
+#Matching the earliest existence of S1[2] in S2
 if S1[2] == S2[0] and ((S2[0] != (S1[0] or S1[1])) or (S2[1] != (S1[0] or S1[1]))):
     A = A + S2[0]
 elif S1[2] == S2[1] and ((S2[1] != (S1[1] or S1[2])) or (S2[2] != (S1[1] or S1[2]))):
@@ -50,6 +53,7 @@ else:
     A = A
 print(A)
 
+#Matching the earliest existence of S1[3] in S2
 if S1[3] == S2[0] and ((S2[0] != (S1[0] or S1[1] or S1[2])) or (S2[1] != (S1[0] or S1[1] or S1[2])) or (S2[2] != (S1[0] or S1[1] or S1[2]))):
     A = A + S2[0]
 elif S1[3] == S2[1] and ((S2[1] != (S1[1] or S1[2] or S1[3])) or (S2[2] != (S1[1] or S1[2] or S1[3])) or (S2[3] != (S1[1] or S1[2] or S1[3]))):
@@ -66,6 +70,7 @@ else:
     A = A
 print(A)
 
+#Matching the earliest existence of S1[4] in S2
 if S1[4] == S2[0] and ((S2[0] != (S1[0] or S1[1] or S1[2] or S1[3])) or (S2[1] != (S1[0] or S1[1] or S1[2] or S1[3])) or
 (S2[2] != (S1[0] or S1[1] or S1[2] or S1[3])) or (S2[3] != (S1[0] or S1[1] or S1[2] or S1[3]))):
     A = A + S2[0]
@@ -86,6 +91,7 @@ else:
     A = A
 print(A)
 
+#Matching the earliest existence of S1[5] in S2
 if S1[5] == S2[0] and ((S2[0] != (S1[0] or S1[1] or S1[2] or S1[3] or S1[4])) or (S2[1] != (S1[0] or S1[1] or S1[2] or S1[3] or S1[4])) or (S2[2] != (S1[0] or S1[1] or S1[2] or S1[3] or S1[4])) or (S2[3] != (S1[0] or S1[1] or S1[2] or S1[3] or S1[4])) or (S2[4] != (S1[0] or S1[1] or S1[2] or S1[3] or S1[4]))):
     A = A + S2[0]
 elif S1[5] == S2[1] and ((S2[1] != (S1[1] or S1[2] or S1[3] or S1[4] or S1[5])) or (S2[2] != (S1[1] or S1[2] or S1[3] or S1[4] or S1[5])) or (S2[3] != (S1[1] or S1[2] or S1[3] or S1[4] or S1[5])) or (S2[4] != (S1[1] or S1[2] or S1[3] or S1[4] or S1[5])) or (S2[5] != (S1[1] or S1[2] or S1[3] or S1[4] or S1[5]))):
--- Exercise2.py
+++ Exercise2.py
@@ -1,45 +1,12 @@
-# =============================================================================
-# A = "ABBD"
-# B = "AB*D"
-# print(B)
-# 
-# if A[1] == A[2]:
-#     A[2] == B[2]
-#     print(A is B)
-# =============================================================================
-
-# =============================================================================
-# A = "AA"
-# B = "A*"
-# if A[0] == A[1] and A[0] == B[0]:
-#     B = B.replace(B[1], A[1])
-# # =============================================================================
-# # else:
-# #     A[1] != B[1]
-# # =============================================================================
-# print(B)
-# print(A is B)
-# =============================================================================
-# =============================================================================
-# A = str(input("Enter string A: "))
-# B = str(input("Enter string B: "))
-# 
-# for i in range(0,len(B)):
-#     if B[i] == ".":
-#         B = B.replace(B[i], B[i-1])
-#         print(B)
-#     if B[i] == "*":
-#         B = B.replace(B[i], B[i-1])
-#         print(B)
-# print(B == A)
-# =============================================================================
-
+#Entering the 2 string values for A and B
 A = str(input("Input values for A: "))
 B = str(input("Input values for B: "))
 C = abs(len(A) - len(B))
 print(C)
 
-
+#Putting a condition for * to replace it with the previous character
+#If A and B have similar lengths or if B is longer than A, then * will be replaced only once
+#If A is longer than B, then * will be replaced with multiple instances of previous values until the lengths of A and B become equal
 for i in range(0, len(B)):
     
     if B[i] == "*":
@@ -50,7 +17,7 @@ for i in range(0, len(B)):
         print("Now B is ", B)
         
         
-
+#Putting a condition to replace "." with previous character
 for i in range(0, len(B)):   
     if B[i] == ".":
         B = B.replace(B[i], B[i-1], 1)
@@ -58,6 +25,7 @@ for i in range(0, len(B)):
         B = B
     print("Now B is ", B)
 
+#If the string B begins with a value of ".", then replace it with the first index of A
 if B[0] == ".":
     B = B.replace(B[0], A[0])
 print("Now B is ", B)
